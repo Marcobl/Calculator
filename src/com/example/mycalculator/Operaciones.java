cf193e834ffe43b7960154ee11985d0fc9fb3705
Marco Andres Bertel Lozano <marco.bertel@gmail.com>
2013-06-17 22:41:52 -0500

    optimizada clase Operaciones

--- src/com/example/mycalculator/Operaciones.java
+++ src/com/example/mycalculator/Operaciones.java
@@ -1,45 +1,33 @@
 package com.example.mycalculator;
 
 public class Operaciones {
-	
-	
-	
-	public static String calcular_operacion(String a, String b, String signo){
+
+	public static String calcular_operacion(String a, String b, String signo) {
 		a.trim();
 		b.trim();
-		/*if (a.equals("")||b.equals("")) {
-			a="0";
-			b="0";
-		}*/
-				
-		if (signo.equals("+")) {			
-			 
-			return String.valueOf((Integer.parseInt(a)+Integer.parseInt(b))) ;
-			
+		/*
+		 * if (a.equals("")||b.equals("")) { a="0"; b="0"; }
+		 */
+
+		if (signo.equals("+")) {
+
+			return String.valueOf((Integer.parseInt(a) + Integer.parseInt(b)));
+
+		} else if (signo.equals("-")) {
+
+			return String.valueOf((Integer.parseInt(a) - Integer.parseInt(b)));
+
+		} else if (signo.equals("*")) {
+
+			return String.valueOf((Integer.parseInt(a) * Integer.parseInt(b)));
+
 		} else {
-			if (signo.equals("-")) {				
-				 
-				return  String.valueOf((Integer.parseInt(a)-Integer.parseInt(b)));
-				
-			} else {
-				if (signo.equals("*")) {					
-					
-					return  String.valueOf((Integer.parseInt(a)*Integer.parseInt(b)));
-					
-				} else {					
-					
-					return  String.valueOf((Double.parseDouble(a)/Double.parseDouble(b)));
 
-				}
-
-			}
+			return String.valueOf((Double.parseDouble(a) / Double
+					.parseDouble(b)));
 
 		}
-				
-				
-		
+
 	}
-	
-	
 
 }
