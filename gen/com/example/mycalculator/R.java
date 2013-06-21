c9bff0a97ff438cf8ee2bb58f0ec0421c6ed7a3a
Marco Andres Bertel Lozano <marco.bertel@gmail.com>
2013-06-20 20:59:17 -0500

    Mejoras a la interfaz

--- gen/com/example/mycalculator/R.java
+++ gen/com/example/mycalculator/R.java
@@ -10,6 +10,10 @@ package com.example.mycalculator;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int DimGray=0x7f040001;
+        public static final int White=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,64 +21,64 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int borrar=0x7f020000;
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080016;
-        public static final int button_cero=0x7f080014;
-        public static final int button_cinco=0x7f08000a;
-        public static final int button_cuatro=0x7f080009;
-        public static final int button_division=0x7f080011;
-        public static final int button_dos=0x7f080005;
-        public static final int button_multriplicar=0x7f08000c;
-        public static final int button_nueve=0x7f080010;
-        public static final int button_ocho=0x7f08000f;
-        public static final int button_resta=0x7f080007;
-        public static final int button_resultado=0x7f080015;
-        public static final int button_seis=0x7f08000b;
-        public static final int button_siete=0x7f08000e;
-        public static final int button_suma=0x7f080002;
-        public static final int button_tres=0x7f080006;
-        public static final int button_uno=0x7f080004;
-        public static final int editT_resultado=0x7f080001;
-        public static final int imageView_borrar=0x7f080013;
-        public static final int tableRow1=0x7f080000;
-        public static final int tableRow2=0x7f080003;
-        public static final int tableRow3=0x7f080008;
-        public static final int tableRow4=0x7f08000d;
-        public static final int tableRow5=0x7f080012;
+        public static final int action_settings=0x7f090016;
+        public static final int button_cero=0x7f090014;
+        public static final int button_cinco=0x7f09000a;
+        public static final int button_cuatro=0x7f090009;
+        public static final int button_division=0x7f090011;
+        public static final int button_dos=0x7f090005;
+        public static final int button_multriplicar=0x7f09000c;
+        public static final int button_nueve=0x7f090010;
+        public static final int button_ocho=0x7f09000f;
+        public static final int button_resta=0x7f090007;
+        public static final int button_resultado=0x7f090015;
+        public static final int button_seis=0x7f09000b;
+        public static final int button_siete=0x7f09000e;
+        public static final int button_suma=0x7f090002;
+        public static final int button_tres=0x7f090006;
+        public static final int button_uno=0x7f090004;
+        public static final int editT_resultado=0x7f090001;
+        public static final int imageView_borrar=0x7f090013;
+        public static final int tableRow1=0x7f090000;
+        public static final int tableRow2=0x7f090003;
+        public static final int tableRow3=0x7f090008;
+        public static final int tableRow4=0x7f09000d;
+        public static final int tableRow5=0x7f090012;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int borrar=0x7f050007;
-        public static final int division=0x7f050006;
-        public static final int hello_world=0x7f050002;
-        public static final int multipl=0x7f050005;
-        public static final int num_cero=0x7f050009;
-        public static final int num_cinco=0x7f05000e;
-        public static final int num_cuatro=0x7f05000d;
-        public static final int num_dos=0x7f05000b;
-        public static final int num_nueve=0x7f050012;
-        public static final int num_ocho=0x7f050011;
-        public static final int num_seis=0x7f05000f;
-        public static final int num_siete=0x7f050010;
-        public static final int num_tres=0x7f05000c;
-        public static final int num_uno=0x7f05000a;
-        public static final int resta=0x7f050004;
-        public static final int resultado=0x7f050008;
-        public static final int suma=0x7f050003;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int borrar=0x7f060007;
+        public static final int division=0x7f060006;
+        public static final int hello_world=0x7f060002;
+        public static final int multipl=0x7f060005;
+        public static final int num_cero=0x7f060009;
+        public static final int num_cinco=0x7f06000e;
+        public static final int num_cuatro=0x7f06000d;
+        public static final int num_dos=0x7f06000b;
+        public static final int num_nueve=0x7f060012;
+        public static final int num_ocho=0x7f060011;
+        public static final int num_seis=0x7f06000f;
+        public static final int num_siete=0x7f060010;
+        public static final int num_tres=0x7f06000c;
+        public static final int num_uno=0x7f06000a;
+        public static final int resta=0x7f060004;
+        public static final int resultado=0x7f060008;
+        public static final int suma=0x7f060003;
     }
     public static final class style {
         /** 
@@ -98,10 +102,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
